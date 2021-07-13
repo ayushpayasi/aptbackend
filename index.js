bba31d2227966b3ea95a86e54c66d9b52be1b339
Ayush Payasi <ayushpayasi@gmail.com>
2021-07-13 23:59:23 +0530

    resolved linux error -Ayush

--- index.js
+++ index.js
@@ -5,37 +5,8 @@ const axios = require("axios");
 const nodemailer = require("nodemailer");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
-// const { uploadFile } = require("./s3");
+const { uploadFile } = require("./s3");
 const sgMail = require('@sendgrid/mail');
-require('dotenv').config()
-const fs = require("fs")
-const S3 = require("aws-sdk/clients/S3")
-
-
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_KEY
-
-const s3 = new S3({
-region,
-accessKeyId,
-secretAccessKey
-})
- 
-function uploadFile(file){
-    const fileStream = fs.createReadStream(file.path)
-
-    const uploadParams = {
-        Bucket: bucketName,
-        Body: fileStream,
-        Key: file.filename
-    }
-
-    return s3.upload(uploadParams).promise()
-
-}
-
 
 
 const app = express();
--- s3.js
+++ s3.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const fs = require("fs")
-const S3 = require("aws-sdk/clients/S3")
+const S3 = require("aws-sdk/clients/s3")
 
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
